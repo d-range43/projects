3203f001ddb8a161af3d1c4b0316cc8194333223
d-range43 <152213195+d-range43@users.noreply.github.com>
2024-03-17 14:09:06 +0000

    Add files via upload

--- test_morsewc.py
+++ test_morsewc.py
@@ -1,10 +1,20 @@
 from morsewc import EnglishMessage, MorseMessage
 
+# Create string to be tested
 message_string = "hello everyone"
+
+# Initialise message object
 message = EnglishMessage(message_string)
 
+# Encodes message to morse
 code_string = message.encode()
+
+# Initalise code object
 code = MorseMessage(code_string)
+
+# Decodes code to english
 decoded = code.decode()
 
+# Check if orginal message == decoded message,
+#if True then morsewc works as intended.
 print(decoded == message_string)
